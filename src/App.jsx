67abcfd561755540ff6f38286d0787842cf61dd6
David Štafa <david.stafa@gmail.com>
2023-11-03 13:25:54 +0100

    Search bar, basic roating, pagination, context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+// import "./App.css";
+import SearchBar from "./david/SearchBar";
+import Context from "./david/context/Context";
+import { useReducer } from "react";
+import Reducer from "./david/context/reducer";
 
 function App() {
-  return <></>;
+
+  const[contextValue, setContextValue]=useReducer( Reducer, {
+    searchValue: (''),
+    searchData: [],
+    page: 1,
+    per_page: 12,
+    // currency: localStorage.getItem('currency') || 'EUR',
+  })
+
+  return (
+    <BrowserRouter >
+      <Context.Provider value={{ state: contextValue, dispatch: setContextValue }} >
+        <>
+          <SearchBar />
+
+        </>
+      </Context.Provider >
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/david/context/reducer.js
@@ -0,0 +1,31 @@
+export default function Reducer(state, action) {
+  
+    // console.log(state);
+    // console.log(action);
+
+  switch (action.type) {
+    case "searchValue/set":
+      return {
+        ...state,
+        searchValue: action.payload,
+      };
+    case "searchData/set":
+      return {
+        ...state,
+        searchData: action.payload,
+      };
+    case "page/previous":
+      return {
+        ...state,
+        page: action.payload,
+      };
+    case "page/next":
+      return {
+        ...state,
+        page: action.payload,
+      };
+
+    default:
+      break;
+  }
+}
