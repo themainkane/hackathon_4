8945af381e3b230462830dc1c35a9dee5dc82c12
Damaris Jean <Damarisjjm@icloud.com>
2023-11-03 14:25:29 +0100

    damaris randomimg

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,38 @@ import SearchBar from "./david/SearchBar";
 import Context from "./david/context/Context";
 import { useReducer } from "react";
 import Reducer from "./david/context/reducer";
-import Images from "./damaris/Images";
+// import Images from "./damaris/Images";
 import Author from "./joe-components/Author";
 import Footer from "./jakub/Footer";
 import Header from "./jakub/Header";
-
+import ImgRandom from "./damaris/ImgRandom";
 
 function App() {
-
-  const[contextValue, setContextValue]=useReducer( Reducer, {
-    searchValue: (''),
+  const [contextValue, setContextValue] = useReducer(Reducer, {
+    searchValue: "",
     searchData: [],
     page: 1,
     per_page: 12,
     // currency: localStorage.getItem('currency') || 'EUR',
-  })
+  });
 
   return (
-    <BrowserRouter >
-      <Context.Provider value={{ state: contextValue, dispatch: setContextValue }} >
+    <BrowserRouter>
+      <Context.Provider
+        value={{ state: contextValue, dispatch: setContextValue }}
+      >
         <>
-
-         <Header />
+          <Header />
           <SearchBar />
-          <Images />
+          {/* <Images /> */}
+
           <Author />
+          <ImgRandom />
 
-          
           <Footer />
-
         </>
-      </Context.Provider >
+      </Context.Provider>
     </BrowserRouter>
-  )
+  );
 }
 export default App;
