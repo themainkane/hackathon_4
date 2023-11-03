a842317bf793307c88108d000f17195087482ba7
Jakub Kozler <kozlerjakub@gmail.com>
2023-11-03 14:14:14 +0100

    reviewed structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 // import "./App.css";
-import SearchBar from "./david/SearchBar";
 import Context from "./david/context/Context";
 import { useReducer } from "react";
 import Reducer from "./david/context/reducer";
-import Images from "./damaris/Images";
-import Author from "./joe-components/Author";
-import Footer from "./jakub/Footer";
-import Header from "./jakub/Header";
+import Homepage from "./Homepage";
 
 
 function App() {
@@ -24,15 +20,7 @@ function App() {
     <BrowserRouter >
       <Context.Provider value={{ state: contextValue, dispatch: setContextValue }} >
         <>
-
-         <Header />
-          <SearchBar />
-          <Images />
-          <Author />
-
-          
-          <Footer />
-
+          <Homepage />
         </>
       </Context.Provider >
     </BrowserRouter>
