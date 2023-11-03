69129800902f96d0e98cd34397b6f410354c5645
themainkane <140316797+themainkane@users.noreply.github.com>
2023-11-03 09:07:23 +0000

    Delete keys.js

--- keys.js
+++
@@ -1,9 +0,0 @@
-const joeKey = "G_aby2Z5oMolServnUDUmliQg3I0s_uuQiXx_3SWslo";
-
-const davidKey = "CGY6v9z58MdS2hv2OPpArYt2izK35qvf0WnJDYRUsG4";
-
-const damarisKey = "9fZShGmtvLCEHG4KhJarL6qQOJFEZI75FqEWu8D4iaE";
-
-const jakubKey = "lsJ_v19i9tjUohM0TcigIvXKTcsg4C7janOMruH6Tfo";
-
-export { joeKey, davidKey, damarisKey, jakubKey };
